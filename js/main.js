51a315c0964d97ae011ba652c5c3813587cb603e
alanlinares <alan.linares@gmail.com>
2022-12-09 23:04:35 -0500

    cursor updated

--- js/main.js
+++ js/main.js
@@ -18,6 +18,28 @@
 //     }
 
 //}
+// document.body.onmousemove = function(e) {
+//     document.documentElement.style.setProperty (
+//       '--x', (
+//         e.clientX+window.scrollX
+//       )
+//       + 'px'
+//     );
+//     document.documentElement.style.setProperty (
+//       '--y', (
+//         e.clientY+window.scrollY
+//       ) 
+//       + 'px'
+//     );
+//   }
+  $(document).ready(function() {
+    $(document).on('mousemove', function(e) {
+      $('#circularcursor').css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    })
+  });
 
 function myFunction() {
   var x = document.getElementById("nav-container");
